fa8375459896333f7ae66d681df6f57c79c43a5d
dalya03 <dalya.yasser1@gmail.com>
2025-02-28 18:42:49 +0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,29 +1,21 @@
 import express from 'express';
-import HTTP_CODES from './utils/httpCodes.mjs';
-import log from './modules/log.mjs';
-import { LOGG_LEVELS } from './modules/log.mjs';
-import { startSession, updateSession } from './middleware/sessionMiddleware.mjs';
+import { startSession, updateSession } from './modules/sessionMiddleware.mjs';
 import tasksRouter from './routes/tasksRouter.js';
-
-const ENABLE_LOGGING = true;  
 
 const server = express();
 const port = process.env.PORT || 5000;
 
-const logger = log(LOGG_LEVELS.VERBOSE);
-
 server.set('port', port);
-server.use(express.json());  
-server.use(logger);
+
+server.use(express.json());
 server.use(startSession);
-server.use(express.static('public')); 
+server.use(express.static('public'));
 
-
-server.use("/api/tasks", tasksRouter);
+server.use('/api/tasks', tasksRouter);
 
 server.use(updateSession);
 
-server.listen(server.get('port'), function () {
+server.listen(server.get('port'), () => {
     console.log(`✅ Server running on http://localhost:${server.get('port')}`);
 });
 
