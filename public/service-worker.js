41cb65714e67634faa13dd488604675a68fcf24e
dalya03 <dalya.yasser1@gmail.com>
2025-03-07 23:42:41 +0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,29 +1,22 @@
-const CACHE_NAME = 'my-app-cache-v1';
+const CACHE_NAME = 'deck-game-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
     '/css/style.css',
-    '/script.js',
-    '/icon-192x192.png',
-    '/icon-512x512.png'
+    '/js/app.js',
+    '/manifest.json'
 ];
-
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(urlsToCache);
-            })
+            .then((cache) => cache.addAll(urlsToCache))
     );
 });
-
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
-            })
+            .then((response) => response || fetch(event.request))
     );
 });
