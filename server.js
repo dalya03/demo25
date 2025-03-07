929ac9e2efc4228ad683b82f3d1670764bc55449
dalya03 <dalya.yasser1@gmail.com>
2025-03-07 23:47:59 +0300

    Update and rename server.mjs to server.js

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 import express from 'express';
-import { startSession, updateSession } from './modules/sessionMiddleware.mjs';
-import tasksRouter from './routes/tasksRouter.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { startSession, updateSession } from './modules/sessionMiddleware.mjs';  
+import tasksRouter from './routes/tasksRouter.js'; 
 
 const server = express();
 const port = process.env.PORT || 5000;
 
 server.set('port', port);
 
+server.use(helmet());
+server.use(morgan('dev'));
 server.use(express.json());
-server.use(startSession);
+server.use(startSession);  
 server.use(express.static('public'));
 
 server.use('/api/tasks', tasksRouter);
-
-server.use(updateSession);
+server.use(updateSession);  
 
 server.listen(server.get('port'), () => {
     console.log(`✅ Server running on http://localhost:${server.get('port')}`);
 });
 
-export default server;
+export default server; 
