3aebf9c5e21660ae923fa8df578bb9cefb96c5fd
dalya03 <dalya.yasser1@gmail.com>
2025-03-09 00:31:13 +0300

    Update tasksRouter.js

--- routes/tasksRouter.js
+++ routes/tasksRouter.js
@@ -3,15 +3,9 @@ import { createDeck, shuffleDeck, showDeck, drawCard } from '../controllers/task
 
 const router = express.Router();
 
-
 router.post('/create-deck', createDeck);
-
-
 router.post('/shuffle-deck/:deckId', shuffleDeck);
-
 router.get('/show-deck/:deckId', showDeck);
-
-
 router.post('/draw-card/:deckId', drawCard);
 
 export default router;
