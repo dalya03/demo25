db629f9a4063cd4e48d0bd84073a8828c75ef6d0
dalya03 <dalya.yasser1@gmail.com>
2025-03-11 23:29:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,19 @@
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import dotenv from 'dotenv';
-import { Pool } from 'pg';
 import { startSession, updateSession } from './modules/sessionMiddleware.mjs';  
-import tasksRouter from './routes/tasksRouter.js';
-
-dotenv.config();
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-});
+import tasksRouter from './routes/tasksRouter.js'; 
 
 const server = express();
 const port = process.env.PORT || 5000;
 
 server.set('port', port);
+
 server.use(helmet());
 server.use(morgan('dev'));
 server.use(express.json());
 server.use(startSession);  
 server.use(express.static('public'));
-
-// Attach DB Pool to Requests
-server.use((req, res, next) => {
-    req.db = pool;
-    next();
-});
 
 server.use('/api/tasks', tasksRouter);
 server.use(updateSession);  
@@ -36,4 +22,4 @@ server.listen(server.get('port'), () => {
     console.log(`✅ Server running on http://localhost:${server.get('port')}`);
 });
 
-export default server;
+export default server; 
